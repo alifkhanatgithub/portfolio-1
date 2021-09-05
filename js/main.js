88034befa09511077d42409f1e4e440fa0a05956
alifkhan <89409450+alifkhanatgithub@users.noreply.github.com>
2021-09-05 22:48:04 +0600

    yes i am starting it
    trying hearder another way

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//mix it up
+var mixer = mixitup('.grid-portofolio-container');
+//scrollify
+
+$(function() {
+  $.scrollify({
+    section : ".scrollify",
+    setHeights: false,
+    updateHash: false,
+  });
+});
